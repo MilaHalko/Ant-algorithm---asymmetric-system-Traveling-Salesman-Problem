68688bf6244efa110b87f15feee869963b51e136
milka_deluxe <milagalko1@gmail.com>
2021-11-24 02:33:33 +0200

    main algorithm + spreadAnts() + setAntType()

--- ant_algorithm/ant_algorithm/main.cpp
+++ ant_algorithm/ant_algorithm/main.cpp
@@ -4,6 +4,8 @@
 #include <time.h>
 using namespace std;
 
+const int ITTERATIONS = 1;
+
 const int SIZE = 10;  // goal 300
 const int MIN_DISTANCE = 2; // goal 5
 const int MAX_DISTANCE = 7; // goal 150
@@ -13,9 +15,14 @@ const float A = 1;
 const float B = 1;
 const float P = 0.2;
 const float Q = 34;   // LMin
-const int S_ANTS = 8; // standard
+const int S_ANTS = 7; // standard
 const int E_ANTS = 2; // elite
-const int W_ANTS = 2; // wild
+const int W_ANTS = 1; // wild
+const int ALL_ANTS = S_ANTS + E_ANTS + W_ANTS;
+
+int s_ants = S_ANTS; // standard
+int e_ants = E_ANTS; // elite
+int w_ants = W_ANTS; // wild
 const bool STATIC_START = true;
 
 
@@ -61,12 +68,103 @@ void coutTable(vector<vector<DistanceAndPheromone>> table) {
     cout << endl;
 }
 
+vector<int> spreadAnts() {                          // |    0    |  1  |  2  |  .. |       ...       | ...     |   all |
+                                                    // | ant = 0 |      s_ants     |      e_ants     |      w_ants     |
+    vector<int> ants (0);
+    int point = s_ants;
+    ants.push_back(point);
+    point += e_ants;
+    ants.push_back(point);
+    point += w_ants;
+    ants.push_back(point);
+    
+    //OPTIONAL
+    cout << "Spread: ";
+    for (auto elem: ants) {cout << elem << "   ";}
+    cout << endl;
+    
+    return ants;
+}
+
+void setAntType(vector<int> antsProportion, bool &S, bool &E, bool &W) {
+    int antType = 0;
+    float percent = rand() % (s_ants + e_ants + w_ants) + 1;
+    
+    for (int i = 0; i < antsProportion.size(); i++) {
+        if (antsProportion[i] >= percent) {
+            antType = i+1;
+            break;
+        }
+    }
+    
+    //OPTIONAL
+    cout << "AntType: " << antType << endl;
+    
+    switch (antType) {
+        case 1:
+            S = true;
+            break;
+        case 2:
+            E = true;
+            break;
+        case 3:
+            W = true;
+            break;
+        default:
+            cout << "ERROR, WRONG ANT TYPE \n";
+            break;
+    }
+}
+
+void colonySearchProcess(vector<vector<DistanceAndPheromone>> table) {
+    int startV = STATIC_START? (rand() % SIZE) : -1;
+    
+    //OPTIONAL
+    cout << "Vertice: " << startV << "\n";
+    
+    for (int i = 0; i < ALL_ANTS; i++) {
+        if (!STATIC_START) {startV = rand() % SIZE;}
+        
+        bool standard = false, elite = false, wild = false;
+        vector<int> antsProportion = spreadAnts();
+        setAntType(antsProportion, standard, elite, wild);
+        
+        if(standard) {
+            //OPTIONAL
+            cout << "standard\n";
+            s_ants--;
+        }
+        if (elite) {
+            //OPTIONAL
+            cout << "elite\n";
+            e_ants--;
+        }
+        if (wild) {
+            //OPTIONAL
+            cout << "wild\n";
+            w_ants--;
+        }
+        
+        //OPTIONAL
+        cout << "All ants: " << s_ants << " + " << e_ants << " + " << w_ants << " = " << s_ants + e_ants + w_ants << endl << endl;
+    }
+}
+
 int main() {
     srand(static_cast<unsigned int>(time(0)));
     vector<vector<DistanceAndPheromone>> table (0, vector<DistanceAndPheromone>(0)); //table of distances and pheromones
     //generateDistances(table);
     fullTable(table);
+    
+    //OPTIONAL
     coutTable(table);
+    
+    for (int i = 0; i < ITTERATIONS; i++) {
+        s_ants = S_ANTS;
+        e_ants = E_ANTS;
+        w_ants = W_ANTS;
+        colonySearchProcess(table);
+    }
     
     return 0;
 }
